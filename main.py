71992593df213bd50814c10fd9a9a8f3ab39609b
ReyChiaro <1402850762@qq.com>
2026-05-25 14:25:49 +0800

    update env

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def main():
-    print("Hello from editonline!")
+import uvicorn
+
+
+def main() -> None:
+    uvicorn.run("editonline.app:app", host="127.0.0.1", port=8000, reload=True)
 
 
 if __name__ == "__main__":
